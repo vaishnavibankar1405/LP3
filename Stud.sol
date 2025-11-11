400f8c1d1fd511b8d416e9c7864fe0a74fe24c25
vaishnavibankar1405 <vaishnavibankar50@gmail.com>
2025-11-11 07:18:25 +0530

    Create Stud.sol

---
+++ Stud.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract StudentData{
+    struct Student{
+        string name;
+        uint rollno;
+    }
+
+    Student[] public studentArr;
+
+    function addStudent(string memory name,uint rollno) public{
+        for(uint i=0;i<studentArr.length;i++){
+            if(studentArr[i].rollno==rollno){
+                revert("Student with roll no already exists!");
+            }
+        }
+        studentArr.push(Student(name,rollno));
+    }
+
+    function displayAllStudents() public view returns(Student[] memory){
+        return studentArr;
+    }
+
+    function getStudentByIndex(uint index) public view returns(Student memory){
+        require(index<studentArr.length || index > studentArr.length,"Index out of bound");
+        return studentArr[index];
+    }
+
+    function getLengthOfStudents() public view returns(uint){
+        return studentArr.length;
+    }
+
+
+}
